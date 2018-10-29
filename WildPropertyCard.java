887bd423b66bc88151356232afb5fb6f24166b6e
NickSoetaert <nicksoetaert@gmail.com>
2018-10-28 18:59:41 -0700

    cleaned up initializer

--- WildPropertyCard.java
+++ WildPropertyCard.java
@@ -24,27 +24,43 @@ public class WildPropertyCard extends PropertyCard {
 	colors.add(color2);
 
 	//currColor stays null until played
-        if(color1 == PropertyColor.BLUE || color2 == PropertyColor.BLUE){
+        if(colors.contains(PropertyColor.BLUE) &&
+	   colors.contains(PropertyColor.GREEN)){
             _name = "Blue/Green Property Wild Card";
             _value = 4;
-        } else if (color1 == PropertyColor.BROWN || color2 == PropertyColor.BROWN){
+        } 
+	else if (colors.contains(PropertyColor.BROWN) &&
+		 colors.contains(PropertyColor.BLUE)){
 	    _name = "Light Blue/Brown Property Wild Card";
 	    _value = 1;
-        } else if (color1 == PropertyColor.ORANGE || color2 == PropertyColor.ORANGE){
+        } 
+	else if (colors.contains(PropertyColor.ORANGE) &&
+		 colors.contains(PropertyColor.PINK)){
 	    _name = "Orange/Pink Property Wild Card";
 	    _value = 2;
-        } else if (color1 == PropertyColor.GREEN || color2 == PropertyColor.GREEN){
+        } 
+	else if (colors.contains(PropertyColor.GREEN) && 
+		 colors.contains(PropertyColor.RAIL)){
 	    _name = "Green/Rail Property Wild Card";
 	    _value = 4;
-	    //we're leaving out light blue/rail
-        } else if (color1 == PropertyColor.UTIL || color2 == PropertyColor.UTIL){
+        }
+	else if (colors.contains(PropertyColor.LIGHT_BLUE) && 
+		 colors.contains(PropertyColor.RAIL)){
+	    _name = "Light Blue/Rail Property Wild Card";
+	    _value = 4;
+	}
+	else if (colors.contains(PropertyColor.RAIL) &&
+		 colors.contains(PropertyColor.UTIL)){
 	    _name = "Rail/Util Property Wild Card";
 	    _value = 2;
-        } else if (color1 == PropertyColor.YELLOW || color2 == PropertyColor.YELLOW){
+        }
+	else if (colors.contains(PropertyColor.YELLOW) &&
+		 colors.contains(PropertyColor.RED)){
 	    _name = "Yellow/Red Property Wild Card";
 	    _value = 3;
-        } else {
-	    System.out.println("WildPropertyCard.java: Trying to create bad prop wild card.");
+        }
+	else {
+	    System.out.println("ERROR - Trying to create a bad property wild card.");
         }
     }
 
